a3047862f35c5b5a05f0ccb2ac9a34b562cc989b
m11tt <2310826859@qq.com>
2025-12-16 20:29:36 +0800

    Update menuD_SystemSetting.c

--- studentmanager/menuD_SystemSetting.c
+++ studentmanager/menuD_SystemSetting.c
@@ -1,3 +1,13 @@
 #include<stdio.h>
 #include"header.h"
 
+void userSetting()          //用户设置 
+{
+      
+}
+void subjectSetting()       //学科设置
+{
+    
+}
+void grade_classSetting();    //�꼶���༶���� 
+void grade_standardSetting(); //�ɼ���׼���� 
